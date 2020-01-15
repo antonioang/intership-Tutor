3a675bb653ad7cc71e3b88a74e33006e8fee8667
Mattyu Lenza <mattia.lenza@student.univaq.it>
2020-01-15 17:39:55 +0100

    dettaglio tirocinio fix, isLogged attivo su tutte le pagine, creazione servlet RichiediTirocinio

---
+++ src/main/java/Controller/RichiediTirocinio.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author matti
+ */
+public class RichiediTirocinio extends BaseController {
+    
+    @Override
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+    }
+
+   
+}
