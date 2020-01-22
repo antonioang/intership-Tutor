711211f15e3803231bae37522c478172cbbb06d0
Mattyu Lenza <mattia.lenza@student.univaq.it>
2020-01-22 22:16:06 +0100

    fix Servlet UploadDocumenti + fix datasource non trovato

--- src/main/java/Controller/UploadDocumenti.java
+++ src/main/java/Controller/UploadDocumenti.java
@@ -12,11 +12,17 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.Iterator;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import javax.servlet.http.Part;
+import org.apache.tomcat.util.http.fileupload.FileItem;
+import org.apache.tomcat.util.http.fileupload.FileUploadException;
+import org.apache.tomcat.util.http.fileupload.disk.DiskFileItemFactory;
+import org.apache.tomcat.util.http.fileupload.servlet.ServletFileUpload;
+import org.apache.tomcat.util.http.fileupload.servlet.ServletRequestContext;
 
 /**
  *
@@ -31,100 +37,120 @@ public class UploadDocumenti extends BaseController {
         HttpSession s = SecurityLayer.checkSession(request);
         if (s!= null) {
             request.setAttribute("username", (String)s.getAttribute("username"));
-            request.setAttribute("tipo", (String)s.getAttribute("tipo"));
+            request.setAttribute("tipo", (int)s.getAttribute("tipo"));
         }
-        System.out.println("fuori");
-        if (request.getParameter("tipo") != null ) {
-            System.out.println("dentro");
-            if(request.getParameter("tipo").equals("1")){
-                //upload convenzione
-                action_upload_convenzione(request, response);
+        if (request.getContentType() != null && request.getContentType().startsWith("multipart/form-data")) {
+            try {
+                int tipo = 0;
+                int id_azienda = 0;
+                int id_tirocinio = 0;
+                int id_studente = 0;
+                FileItem fileToUpload = null;
+                
+                //Vecchio metodo per estrarre le parti dalle form multipart
+                DiskFileItemFactory factory = new DiskFileItemFactory();
+                ServletFileUpload upload = new ServletFileUpload(factory);
+                List<FileItem> items = upload.parseRequest(new ServletRequestContext(request));
+                Iterator<FileItem> iter = items.iterator();
+                
+                while (iter.hasNext()) {
+                    FileItem item = iter.next();
+
+                    if (item.isFormField() && item.getFieldName().equals("tipo")) {
+                        tipo = SecurityLayer.checkNumeric(item.getString());
+                    }
+                    if (item.isFormField() && item.getFieldName().equals("azienda")) {
+                        id_azienda = SecurityLayer.checkNumeric(item.getString());
+                    }
+                    if (item.isFormField() && item.getFieldName().equals("tirocinio")) {
+                        id_tirocinio = SecurityLayer.checkNumeric(item.getString());
+                    }
+                    if (item.isFormField() && item.getFieldName().equals("studente")) {
+                        id_studente = SecurityLayer.checkNumeric(item.getString());
+                    }
+                    if(item.getFieldName().equals("file") && item.getContentType().equals("application/pdf")){
+                        System.out.println("trovato il file");
+                        System.out.println(item.getContentType());
+                        fileToUpload = item;
+                    }
+                }
+                
+                if (tipo == 1 || tipo == 2 ) {
+                    if(tipo == 1){
+                        //upload convenzione
+                        action_upload_convenzione(request, response, id_azienda, fileToUpload);
+                    }
+                    else if(tipo == 2){
+                        //upload richiesta tirocinio
+                        action_upload_richiesta(request, response, id_studente, id_tirocinio, fileToUpload);
+                    }
+                }
+                else{
+                    request.setAttribute("errore", "errore_caricamento");
+                    request.setAttribute("message", "Impossibile caricare la pagina! Riprova");
+                    action_error(request, response);
+                }
+            } catch (FileUploadException ex) {
+                request.setAttribute("eccezione", ex);
+                action_error(request, response);
             }
-            else if(request.getParameter("tipo").equals("2")){
-                //upload richiesta tirocinio
-                action_upload_richiesta(request, response);
-            }
-        }
-        else{
-            request.setAttribute("errore", "errore_caricamento");
-            request.setAttribute("message", "Impossibile caricare la pagina! Riprova");
-            action_error(request, response);
         }
     }
     
-    private void action_upload_convenzione(HttpServletRequest request, HttpServletResponse response){
-        if(request.getParameter("azienda")!=null && !request.getParameter("azienda").equals("0")){
+    private void action_upload_convenzione(HttpServletRequest request, HttpServletResponse response, int id_azienda, FileItem fileToUpload){
             try {
-                //ottengo l'id dell'azienda 
-                int id_azienda = SecurityLayer.checkNumeric(request.getParameter("azienda"));
-
-                Part file = request.getPart("file");
-                //controllo che il file da caricare sia un pdf
-                if (file.getContentType().equals("application/pdf")) {
-                    //creo un file con un nome univoco
-                    File uploaded = File.createTempFile("convenzione_",  id_azienda +".pdf", new File(getServletContext().getRealPath("") + File.separatorChar + getServletContext().getInitParameter("uploads.directory")));
-                    InputStream is = file.getInputStream();
+                if(fileToUpload != null && id_azienda !=0){
+                    //Controllo che esista la cartella per l'upload
+                    File uploadDir = new File(getServletContext().getRealPath("") + File.separatorChar + getServletContext().getInitParameter("uploads.directory"));
+                    //Se non esiste la creo
+                    if (!uploadDir.exists()) uploadDir.mkdir();
+                    //Creo il file nel percorso specificato
+                    File uploaded = new File(getServletContext().getRealPath("") + File.separatorChar + getServletContext().getInitParameter("uploads.directory")+ File.separatorChar +"convenzione_" + id_azienda +".pdf");
+                    //Ottengo l'input stream dall'elemento della form
+                    InputStream is = fileToUpload.getInputStream();
+                    //Ottengo l'output stream dal file creato in precedenza
                     OutputStream os = new FileOutputStream(uploaded);
-                    //leggo il file dall'input stream e lo scrivo nell'outputstream
+                    //Leggo il file dall'input stream e lo scrivo nell'outputstream
                     byte[] buffer = new byte[1024];
                     int read;
                     while ((read = is.read(buffer)) > 0) {
                         os.write(buffer, 0, read);
                     }
-                    //rimando alla servlet per la gestione delle richieste di convenzione per effettuare update dellla src del documento di convenzione
+                    //Rimando alla servlet per la gestione delle richieste di convenzione per effettuare update dellla src del documento di convenzione
                     response.sendRedirect("gestione_azienda?id="+id_azienda+"&src="+uploaded.getName());
-                }
+                }  
             } catch (IOException ex) {
-                request.setAttribute("eccezione", ex);
-                action_error(request, response);
-            } catch (ServletException ex) {
                 request.setAttribute("eccezione", ex);
                 action_error(request, response);
             }
-        }
     }
     
-    private void action_upload_richiesta(HttpServletRequest request, HttpServletResponse response){
-        if(request.getParameter("studente")!=null && !request.getParameter("studente").equals("0") && 
-            request.getParameter("tirocinio")!=null && !request.getParameter("tirocinio").equals("0")){
-            
-            //ottengo lo studente e il tirocinio dall'id
-            int id_studente = SecurityLayer.checkNumeric(request.getParameter("studente"));
-            int id_tirocinio = SecurityLayer.checkNumeric(request.getParameter("tirocinio"));
-            try {
-                Part file = request.getPart("file");
-                //controllo che il file da caricare sia un pdf
-                if (file.getContentType().equals("application/pdf")) {
-
-                        //creo un file con un nome univoco
-                        File uploaded = File.createTempFile("convenzione_",  id_studente + id_tirocinio +".pdf", new File(getServletContext().getRealPath("") + File.separatorChar + getServletContext().getInitParameter("uploads.directory")));
-                        InputStream is = file.getInputStream();
-                        OutputStream os = new FileOutputStream(uploaded);
-                        //leggo il file dall'input stream e lo scrivo nell'outputstream
-                        byte[] buffer = new byte[1024];
-                        int read;
-                        while ((read = is.read(buffer)) > 0) {
-                            os.write(buffer, 0, read);
-                        }
-                        //rimando alla servlet per la gestione della richiesta tirocinio per aggiornare la src del documento e sbloccare il tasto accetta
-                        response.sendRedirect("gestione_candidati?upload="+uploaded.getName()+"&studente="+id_studente+"&tirocinio="+id_tirocinio);
-
-                }
+    private void action_upload_richiesta(HttpServletRequest request, HttpServletResponse response, int id_studente, int id_tirocinio, FileItem fileToUpload){
+        try {
+                if(fileToUpload != null && id_studente != 0 && id_tirocinio !=0){
+                    //Controllo che esista la cartella per l'upload
+                    File uploadDir = new File(getServletContext().getRealPath("") + File.separatorChar + getServletContext().getInitParameter("uploads.directory"));
+                    //Se non esiste la creo
+                    if (!uploadDir.exists()) uploadDir.mkdir();
+                    //Creo il file nel percorso specificato
+                    File uploaded = new File(getServletContext().getRealPath("") + File.separatorChar + getServletContext().getInitParameter("uploads.directory")+ File.separatorChar +"candidatura_" + id_studente + id_tirocinio +".pdf");
+                    //Ottengo l'input stream dall'elemento della form
+                    InputStream is = fileToUpload.getInputStream();
+                    //Ottengo l'output stream dal file creato in precedenza
+                    OutputStream os = new FileOutputStream(uploaded);
+                    //Leggo il file dall'input stream e lo scrivo nell'outputstream
+                    byte[] buffer = new byte[1024];
+                    int read;
+                    while ((read = is.read(buffer)) > 0) {
+                        os.write(buffer, 0, read);
+                    }
+                    //Rimando alla servlet per la gestione delle richieste di convenzione per effettuare update dellla src del documento di convenzione
+                    response.sendRedirect("gestione_richiesta?tirocinio="+id_tirocinio+"&studente="+id_studente+"&src="+uploaded.getName());
+                }  
             } catch (IOException ex) {
                 request.setAttribute("eccezione", ex);
                 action_error(request, response);
-            } catch (ServletException ex) {
-                request.setAttribute("eccezione", ex);
-                action_error(request, response);
-            }    
-
-            
-        }
-        else{
-            request.setAttribute("errore", "errore_caricamento");
-            request.setAttribute("message", "Impossibile caricare la pagina! Riprova");
-            action_error(request, response);
-        }
+            }
     }
     
     private void action_error(HttpServletRequest request, HttpServletResponse response){
