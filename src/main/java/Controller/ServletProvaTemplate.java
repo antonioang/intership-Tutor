1fa24dd61dfd6d0dd7ebfc24882bddbf28446a0e
antonioang <antonioang9@gmail.com>
2020-01-24 17:34:20 +0100

    pagina errore

--- src/main/java/Controller/ServletProvaTemplate.java
+++ src/main/java/Controller/ServletProvaTemplate.java
@@ -91,7 +91,7 @@ public class ServletProvaTemplate extends HttpServlet {
                 Logger.getLogger(ServletProvaTemplate.class.getName()).log(Level.SEVERE, null, ex);
             }
             
-            temp.activate("crea_resoconto.ftl.html",request,response);
+            temp.activate("errore.ftl.html",request,response);
         } catch(TemplateManagerException ex){
             Logger.getLogger(FailureResult.class.getName()).log(Level.SEVERE, null, ex);
         } catch (SQLException ex) {
