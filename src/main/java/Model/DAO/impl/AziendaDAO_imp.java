1d300d3ab3ecf6082b40b40924c382f56a11c7f8
Mattyu Lenza <mattia.lenza@student.univaq.it>
2020-01-24 18:02:05 +0100

    Servlet Statistiche

--- src/main/java/Model/DAO/impl/AziendaDAO_imp.java
+++ src/main/java/Model/DAO/impl/AziendaDAO_imp.java
@@ -18,9 +18,6 @@ import java.sql.Statement;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 
 /**
  *
